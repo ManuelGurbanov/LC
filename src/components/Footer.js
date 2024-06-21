d0bcfe9455eb084c4e330588e0f4ab6791c27395
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-06-21 15:15:14 -0300

    payment filters

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,14 @@ const Footer = () => {
     return (
         <footer className="flex flex-col items-center justify-center w-screen h-32 p-0 mt-12 text-white bg-gray-800">
             <p className='text-sm font-thin'>© 2024 LC Monedas. Todos los derechos reservados.</p>
+            <a 
+                href="#" 
+                className='text-sm font-thin text-blue-400 hover:text-blue-600'
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Ver webmaster
+            </a>
         </footer>
     );
 }
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RadioGroup, Radio } from '@headlessui/react';
 import s0 from '../img/sponsors/payment0.png';
 import s1 from '../img/sponsors/payment1.png';
 import s2 from '../img/sponsors/payment2.png';
@@ -14,22 +15,56 @@ import s11 from '../img/sponsors/payment11.png';
 import s12 from '../img/sponsors/payment12.png';
 import s13 from '../img/sponsors/payment13.png';
 
-const methods = [
-    s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13
-];
+const methodsByCurrency = {
+    arg: [s0, s1, s2, s3, s4],
+    chi: [s6, s7, s8, s9],
+    col: [s9, s10, s11, s12],
+    eurusd: [s3, s5, s13],
+};
 
 const Payment = () => {
+    const [currency, setCurrency] = useState('arg');
+
+    const handleCurrencyChange = (value) => {
+        setCurrency(value);
+    };
+
     return (
         <div className="flex flex-col items-center justify-center w-screen h-[80vh]">
             <h1 className='font-bold text-4xl w-[80vw] text-black mt-8 text-center'>Métodos de Pago</h1>
+
+            <div data-aos="fade-up" data-aos-delay="300" className="flex justify-center mt-10">
+                <fieldset aria-label="Frecuencia de pago">
+                    <RadioGroup
+                        value={currency}
+                        onChange={handleCurrencyChange}
+                        className="grid grid-cols-4 p-1 text-xs font-semibold leading-5 text-center text-black rounded-full gap-x-1"
+                    >
+                        {['arg', 'chi', 'col', 'eurusd'].map((option) => (
+                            <Radio
+                                key={option}
+                                value={option}
+                                className={({ checked }) =>
+                                    checked
+                                        ? 'bg-cardGreen cursor-pointer rounded-full px-2.5 py-1 transition-colors duration-150 overflow-hidden text-black'
+                                        : 'bg-transparent cursor-pointer rounded-full px-2.5 py-1 transition-colors duration-150 overflow-hidden text-black'
+                                }
+                            >
+                                {option.toUpperCase()}
+                            </Radio>
+                        ))}
+                    </RadioGroup>
+                </fieldset>
+            </div>
+
             <section className="flex flex-col items-center justify-start w-[80vw] sm:w-[60vw] flex-grow mt-4 sm:mt-12 text-black gap-1">
-                <section className="grid items-center justify-center w-full h-auto grid-cols-3 gap-4 mt-12 mb-12 text-black sm:mt-0 sm:mb-0 sm:gap-0 sm:m-0 sm:grid-cols-4 justify-items-center">
-                    {methods.map((method, index) => (
+                <section className="grid items-center justify-center w-full h-auto grid-cols-2 gap-4 mt-12 mb-12 text-black sm:mt-0 sm:mb-0 sm:gap-0 sm:m-0 sm:grid-cols-3 justify-items-center">
+                    {methodsByCurrency[currency].map((method, index) => (
                         <img 
                             key={index} 
                             src={method} 
                             alt={`Payment method`} 
-                            className='w-full h-auto p-1 sm:w-28 opacity-30' 
+                            className='w-full h-auto p-1 opacity-60 sm:w-28' 
                         />
                     ))}
                 </section>
