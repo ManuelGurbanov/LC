ee36391677a77f0940219c046972633b940aca80
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-06-27 01:17:36 -0300

    other details

--- src/components/SellCoins.js
+++ src/components/SellCoins.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Social from './Social';
+import Coins from '../img/coins.webp';
+
 
 AOS.init({
   duration: 500,
@@ -13,13 +15,20 @@ function SellCoins() {
     <>
       <section className="flex flex-col items-center justify-start w-[100vw] ">
         <section className="flex flex-col items-center justify-start w-[90vw] min-h-[70vh] mt-28 lg:mt-28 text-black gap-2">
-          <h1 className="text-3xl font-bold sm:text-6xl" data-aos="fade-up" data-aos-delay="100">Vende tus Monedas</h1>
-          
+          <div className="flex items-center justify-center w-full gap-5">
+
+            <img src={Coins} className='w-12 mt-3 sm:w-14' data-aos="fade-up"></img>
+
+            <h1 className="mt-5 text-2xl italic font-black text-center sm:text-6xl sm:mt-0" data-aos="fade-up" data-aos-delay="100">Vende tus Monedas</h1>
+            
+            <img src={Coins} className='w-12 mt-3 sm:w-14' data-aos="fade-up"></img>
+
+          </div>
           <p className="p-4 mt-4 text-xl font-regular" data-aos="fade-right" data-aos-delay="200">
             ¿Queres ganar dinero jugando Fifa?
           </p>
           <p className="p-4 text-xl font-regular" data-aos="fade-left" data-aos-delay="300">
-            Contactate con nosotros, y sumate a nuestro equipo de trabajo siendo proveedor de Monedas de FC24.
+          <span className='text-2xl font-bold'>Contactate</span> con nosotros, y sumate a nuestro equipo de trabajo siendo proveedor de Monedas de FC24.
           </p>
 
           <p className="p-4 text-xl font-regular" data-aos="fade-left" data-aos-delay="300">
