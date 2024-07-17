d0e4cab065654ac73ecc1680ed57843970d4bf93
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-07-17 14:45:32 -0300

    added new cover image

--- src/components/NoticiasEAFC25.js
+++ src/components/NoticiasEAFC25.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
-const imgURL = "https://media.cospanama.com/p/1893ef2477059704be23af1bf8a9f258/adjuntos/324/imagenes/018/688/0018688543/855x0/smart/fc-25-disclaimers-page-featured-thumbnailjpgadaptcrop16x91023wjpg.jpg";
+const imgURL = "https://www.latercera.com/resizer/bJ6VARSl_BfWZ6NMwW3iRO2hpEE=/800x0/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/RNLKOCNLB5DMFMGANXKPN6B4CQ.jpg";
+const img2URL = "https://pbs.twimg.com/media/GSsSHX7WIAABjXv?format=jpg&name=medium";
+
+const leaguesURL = "https://pbs.twimg.com/media/GStBJBhXEAAM-Ru?format=jpg&name=medium";
 
 const NoticiasEAFC25 = () => {
   return (
     <section className='flex flex-col items-center justify-start w-full min-h-dvh'>
       <h1 className='p-4 mt-24 text-4xl font-bold text-center'>Noticias, Filtraciones y Predicciones de EAFC 25</h1>
 
-      <div className='flex flex-col items-center w-full text-center'>
-        <h2 className='mt-10 text-3xl font-bold'>¡Nueva Portada Oficial!</h2>
-        <p className='mt-5 text-2xl'>A la espera de la imagen de la edicion estándar, esta es la portada oficial de la Ultimate.</p>
-        <img className='w-full p-2 mt-5 sm:w-3/5' src={imgURL} alt='EAFC 25' />
+      <div className='flex flex-col items-center w-4/5 text-center sm:flex-row'>
+        <div className='flex flex-col items-center w-full h-full'>
+          <h2 className='mt-10 text-3xl font-bold'>Edición Estándar</h2>
+          <h3 className='mt-2 text-3xl font-regular'>$69,99 USD</h3>
+          <img className='w-full p-2 mt-5 sm:w-3/5' src={img2URL} alt='EAFC 25' />
+        </div>
+        <div className='flex flex-col items-center w-full h-full'>
+          <h2 className='mt-10 text-3xl font-bold'>Edición Ultimate</h2>
+          <h3 className='mt-2 text-3xl font-regular'>$99,99 USD</h3>
+          <img className='w-full p-2 mt-5 sm:w-3/5' src={imgURL} alt='EAFC 25' />
+        </div>
+      </div>
+
+      <div className='flex flex-col items-center w-4/5 mt-10'>
+        <h2 className='text-3xl font-bold'>Ligas Disponibles</h2>
+        <img className='w-full p-2 mt-5 sm:w-3/5' src={leaguesURL} alt='EAFC 25' />
       </div> 
     </section>
   );
