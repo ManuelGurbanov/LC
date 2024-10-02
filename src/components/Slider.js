0129cd2e2789498e82d4eb175e64375cc5f0de2d
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-10-02 13:56:19 -0300

    equipos de clientes

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,23 +5,31 @@ import ArrowRight from '../img/arrows/ArrowRight.svg';
 
 import Social from './Social';
 
+import team1 from '../img/cards/team1.jpg';
+
+import team2 from '../img/cards/team2.jpg';
 
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  /*const nextSlide = () => {
+  const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
   };
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-  }; */
+  };
+
+  const images = [
+    team1,
+    team2,
+  ];
 
   return (
     <main className='flex flex-col items-center justify-start min-h-screen gap-2 p-4 mb-10 rounded-2xl'>
       <h1 className='mt-24 text-4xl font-bold text-center text-white'>Equipos de clientes</h1>
 
-      {/* <div className="relative flex items-center justify-center w-full mx-auto mt-10 sm:w-3/5">
+      <div className="relative flex items-center justify-center w-full mx-auto mt-10 sm:w-3/5">
         <button
           onClick={prevSlide}
           className="absolute left-[-30px] p-2 transform -translate-y-1/2 bg-zinc-700 bg-opacity-75 rounded-full top-1/2 hover:bg-opacity-25"
@@ -39,7 +47,7 @@ const Slider = () => {
         >
           <img src={ArrowRight} alt='ArrowRight' className='w-6' />
         </button>
-      </div> */}
+      </div>
 
       <p className='w-3/4 mt-4 mb-4 text-3xl text-center text-white sm:w-1/2'>
         En este apartado irán los equipos de clientes que adquieran sus monedas.
